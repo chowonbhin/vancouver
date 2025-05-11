55dedc7267781b4b95b32ec444b277ca38d43ee0
홍성필 <zx2880@naver.com>
2025-05-11 19:05:52 +0900

    Feat add sound & Score UI

--- Assets/BaseballScene/Scripts/BatInteractEvent.cs
+++ Assets/BaseballScene/Scripts/BatInteractEvent.cs
@@ -2,71 +2,77 @@ using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
-public class BatInteractEvent : MonoBehaviour
+namespace BaseBallScene
 {
-    float maxSpeed = 10.0f; // 햅틱 최대 강도에 응하는 속도
-    float duration = 0.1f;
-
-    XRGrabInteractable interactable;
-
-
-    private void Start()
+    public class BatInteractEvent : MonoBehaviour
     {
-        interactable = GetComponent<XRGrabInteractable>();
-        if (interactable != null)
+        float maxSpeed = 10.0f; // 햅틱 최대 강도에 응하는 속도
+        float duration = 0.1f;
+
+        XRGrabInteractable interactable;
+        AudioSource audioSource;
+
+        private void Start()
         {
-            interactable.hoverEntered.AddListener(OnEvent);
-            interactable.hoverExited.AddListener(OnEvent);
-            interactable.selectEntered.AddListener(OnEvent);
-            interactable.selectExited.AddListener(OnEvent);
+            audioSource = GetComponent<AudioSource>();
+            interactable = GetComponent<XRGrabInteractable>();
+            if (interactable != null)
+            {
+                interactable.hoverEntered.AddListener(OnEvent);
+                interactable.hoverExited.AddListener(OnEvent);
+                interactable.selectEntered.AddListener(OnEvent);
+                interactable.selectExited.AddListener(OnEvent);
+            }
+
+            var xrorigin = FindObjectOfType<XROrigin>();
+            var XRInputModalityManager = xrorigin.GetComponent<XRInputModalityManager>();
+        }
+        public void OnEvent(SelectExitEventArgs arg0)
+        {
+
         }
 
-        var xrorigin = FindObjectOfType<XROrigin>();
-        var XRInputModalityManager = xrorigin.GetComponent<XRInputModalityManager>();
-    }
-    public void OnEvent(SelectExitEventArgs arg0)
-    {
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("BaseBall"))
+        private void OnCollisionEnter(Collision collision)
         {
-            float collisionForce = collision.relativeVelocity.magnitude;
-            if (collisionForce != 0)
+            var ball = collision.gameObject.GetComponent<Ball>();
+            if (ball != null)
             {
-                float normalizedSpeed = Mathf.Clamp01(collisionForce / maxSpeed);
-
-                foreach (var interactor in interactable.interactorsSelecting)
+                float collisionForce = collision.relativeVelocity.magnitude;
+                if (collisionForce != 0)
                 {
-                    var controllerInteractor = interactor as XRBaseControllerInteractor;
-                    var controller = controllerInteractor.xrController as ActionBasedController;
-                    controller.SendHapticImpulse(normalizedSpeed, duration);
+                    float normalizedSpeed = Mathf.Clamp01(collisionForce / maxSpeed);
+
+                    foreach (var interactor in interactable.interactorsSelecting)
+                    {
+                        var controllerInteractor = interactor as XRBaseControllerInteractor;
+                        var controller = controllerInteractor.xrController as ActionBasedController;
+                        controller.SendHapticImpulse(normalizedSpeed, duration);
+                        audioSource.Play();
+                    }
                 }
             }
         }
-    }
 
-    public void OnEvent(SelectEnterEventArgs arg0)
-    {
-        if (arg0.interactorObject is XRBaseControllerInteractor controllerInteractor)
+        public void OnEvent(SelectEnterEventArgs arg0)
         {
-            var controller = controllerInteractor.xrController as ActionBasedController;
-            if (controller.CompareTag("Right Controller"))
+            if (arg0.interactorObject is XRBaseControllerInteractor controllerInteractor)
             {
-                controller.SendHapticImpulse(0.05f, duration);
+                var controller = controllerInteractor.xrController as ActionBasedController;
+                if (controller.CompareTag("Right Controller"))
+                {
+                    controller.SendHapticImpulse(0.05f, duration);
+                }
             }
         }
-    }
 
-    public void OnEvent(HoverExitEventArgs arg0)
-    {
+        public void OnEvent(HoverExitEventArgs arg0)
+        {
 
-    }
+        }
 
-    public void OnEvent(HoverEnterEventArgs arg0)
-    {
+        public void OnEvent(HoverEnterEventArgs arg0)
+        {
 
+        }
     }
 }
--- Assets/BaseballScene/Scripts/Pitcher.cs
+++ Assets/BaseballScene/Scripts/Pitcher.cs
@@ -1,10 +1,8 @@
 using UnityEngine;
 using UnityEngine.Pool;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR;
 using System.Collections;
 using UnityEngine.InputSystem;
-using System;
+using BaseBallScene;
 
 namespace HP
 {
@@ -14,20 +12,20 @@ namespace HP
         // 투구 시작점과 종료점을 에디터에서 할당
         public Transform startPoint;
         public Transform endPoint;
-        public GameObject ballPref;
+        public Ball ballPref;
         public Transform balls;
         // 공이 목적지에 도달하는데 걸리는 시간 (초)
         public float duration = 1.5f;
         // 공이 자연ㄴ히 사라지는데 걸리는 시간 (초)
         public float liveTime = 6f;
-        private IObjectPool<GameObject> objectPool;
+        private IObjectPool<Ball> objectPool;
 
 
         public InputActionReference throwAction;
 
         void Start()
         {
-            objectPool = new UnityEngine.Pool.ObjectPool<GameObject>(createFunc: () =>
+            objectPool = new UnityEngine.Pool.ObjectPool<Ball>(createFunc: () =>
             {
                 var ball = Instantiate(ballPref, balls, false);
                 ball.gameObject.SetActive(false);
@@ -35,6 +33,7 @@ namespace HP
             },
             actionOnGet: ball =>
             {
+                ball.ResetState();
                 ball.gameObject.SetActive(true);
                 var tr = ball.GetComponent<Transform>();
                 var rb = ball.GetComponent<Rigidbody>();
@@ -70,7 +69,7 @@ namespace HP
             rb.velocity = initialVelocity;
             StartCoroutine(ReturnToPoolAfterTime(ball, liveTime));
         }
-        private IEnumerator ReturnToPoolAfterTime(GameObject ball, float time)
+        private IEnumerator ReturnToPoolAfterTime(Ball ball, float time)
         {
             yield return new WaitForSeconds(time);
             objectPool.Release(ball);
