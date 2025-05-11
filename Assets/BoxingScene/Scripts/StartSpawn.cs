b4079e81873013048a76075388b34f70475c36d0
Boyazid <boyazidbabamurotov@gmail.com>
2025-05-11 16:19:05 +0900

    Hotfix: Fixed double spawn

--- Assets/BoxingScene/Scripts/StartSpawn.cs
+++ Assets/BoxingScene/Scripts/StartSpawn.cs
@@ -5,36 +5,43 @@ using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class StartSpawn : MonoBehaviour
-
 {
-
     public GameObject spawnManager;
     public XRNode controllerNode = XRNode.RightHand;
     private InputDevice device;
 
-    // Start is called before the first frame update
+    private bool buttonPressedLastFrame = false; 
+
     void Start()
     {
         device = InputDevices.GetDeviceAtXRNode(controllerNode);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && spawnManager != null)
         {
-           
             spawnManager.GetComponent<Spawn>().SpawnBall();
         }
+
         if (!device.isValid)
             device = InputDevices.GetDeviceAtXRNode(controllerNode);
-        
-        if (device.IsPressed(InputHelpers.Button.PrimaryButton, out bool isPressed) && isPressed)
+
+        if (device.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPressed))
         {
-            if (spawnManager != null)
+            if (isPressed && !buttonPressedLastFrame)
             {
-                Debug.Log("A button pressed on Oculus controller");
-                spawnManager.GetComponent<Spawn>().SpawnBall();
+                buttonPressedLastFrame = true;
+
+                if (spawnManager != null)
+                {
+                    Debug.Log("A button pressed on Oculus controller");
+                    spawnManager.GetComponent<Spawn>().SpawnBall();
+                }
+            }
+            else if (!isPressed)
+            {
+                buttonPressedLastFrame = false;
             }
         }
     }
